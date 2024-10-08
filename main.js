8638603827f7e4e5fb77a70299394cdc26011ead
kiri931 <44487096+kiri931@users.noreply.github.com>
2024-10-08 03:50:33 +0000

    10月８日やった分

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const todoList = document.getElementById('todoList');
+document.addEventListener("DOMContentLoaded",function(){
+    const todoList = document.getElementById("todoList");
     loadTodos();
-    function saveTodoToLocalStorage(todoText) {
-        let todos = JSON.parse(localStorage.getItem('todos')) || [];
+    function saveTodoToLocalStorage(todoText){
+        let todos= JSON.parse(localStorage.getItem("todos"))||[];
         todos.push(todoText);
-        localStorage.setItem('todos', JSON.stringify(todos));
+        localStorage.setItem("todos",JSON.stringify(todos));
     }
-    function loadTodos() {
-        let todos = JSON.parse(localStorage.getItem('todos')) || [];
-        todos.forEach(function(todo) {
+    function loadTodos(){
+        let todos = JSON.parse(localStorage.getItem("todos"))||[];
+        todos.forEach(function(todo){
             addTodoToList(todo);
         });
     }
-    window.addTodo = function() {
-        const todoInput = document.getElementById('todoInput');
+    window.addTodo=function(){
+        const todoInput= document.getElementById("todoInput");
         const todoText = todoInput.value.trim();
-
-        if (todoText !== "") {
+        if(todoText !==""){
             addTodoToList(todoText);
             saveTodoToLocalStorage(todoText);
-            todoInput.value = '';
-        } else {
-            alert("ToDoを入力してください。");
+            todoInput.value = "";
+        }else{
+            alert("ToDoを入力してください");
         }
     };
-    function addTodoToList(todoText) {
-        const li = document.createElement('li');
+    function addTodoToList(todoText){
+        const li = document.createElement("li");
+        const check = document.createElement("input");
+        check.setAttribute("type","checkbox");
         li.textContent = todoText;
+        li.appendChild(check);
         todoList.appendChild(li);
     }
+    window.deleteList=function(){
+        localStorage.removeItem("todos");
+    }
 });
+
+
